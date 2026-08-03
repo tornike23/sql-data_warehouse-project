222a049ca487d06c06df70ae4611847921d2159d
tornike23 <65595507+tornike23@users.noreply.github.com>
2026-08-03 23:47:17 +0400

    Create views for customers, products, and sales

--- script/gold/ddl.gold.sql
+++ script/gold/ddl.gold.sql
@@ -13,3 +13,65 @@ Usage:
     - These views can be queried directly for analytics and reporting.
 ===============================================================================
 */
+-- Dimension customers in gold
+create view gold.dim_customers as
+
+select 
+	row_number() over(order by cst_id) as customer_key,
+	ci.cst_id as customer_id,
+	ci.cst_key as customer_number,
+	ci.cst_firstname as first_name,
+	ci.cst_lastname as last_name,
+	la.cntry as country,
+	ci.cst_maritial_status as maritial_status,
+	case when ci.cst_gndr != 'n/a' then ci.cst_gndr -- CRM is the Master
+		 else coalesce(ca.gen, 'n/a')
+	end as gender,
+	ca.bdate as birthdate,
+	ci.cst_create_date as create_date
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
+
+
+--Dimension product in gold
+create view gold.dim_products as
+
+select 
+	row_number() over(order by pn.prd_start_dt, pn.prd_key) as product_key,
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as product_category,
+	pc.subcat as product_subcategory,
+	pc.maintenance,
+	pn.prd_cost as cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_date
+from silver.crm_prd_info pn 
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null --to have the latest information not the whole history
+
+
+--Fact sales in gold
+create view gold.fact_sales as 
+
+select 
+	sd.sls_ord_num as order_number,
+	pr.product_key,
+	cu.customer_key,
+	sd.sls_order_dt as order_date,
+	sd.sls_ship_dt as ship_date,
+	sd.sls_due_dt as due_date,
+	sd.sls_sales as sales,
+	sd.sls_quantity as quantity,
+	sd.sls_price as price
+from silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers cu
+on sd.sls_cust_id = cu.customer_id
